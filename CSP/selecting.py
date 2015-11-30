7358e59fea3e1fe355097114d67401d5c793470d
Safak <s.safak.topcu@gmail.com>
2015-11-30 14:33:58 +0200

    End of backtracking

--- CSP/selecting.py
+++ CSP/selecting.py
@@ -17,6 +17,35 @@ def pick_var(assignment, csp):
             index_of_min = min_domain.index(min(min_domain))
             return indexes[index_of_min]
 
-    # todo max(max_restrict/domain)
 
     return None
+
+def pick_var_RD(assignment, csp):
+    # max(max_restrict/domain)
+    indexes = [x for x in range(assignment.__len__()) if assignment[x] == -1]
+    max_restriction = []
+    if indexes.__len__() > 0:
+        max_restriction = [csp[i].neighbors.__len__() for i in indexes]
+    min_domain = []
+    if indexes.__len__() > 0:
+        min_domain = [csp[i].domain.__len__() for i in indexes]
+    if max_restriction.__len__() > 0 and min_domain.__len__() > 0:
+        rd_list = [max_restriction[i]/min_domain[i] for i in range(max_restriction.__len__())]
+        index_of_max = rd_list.index(max(rd_list))
+        return indexes[index_of_max]
+
+    return None
+
+def pick_var_minimum_domain_max_restrict(assignment, csp):
+    # not bad but, second returns 6... although 5th returns 20 :)
+    indexes = [x for x in range(assignment.__len__()) if assignment[x] == -1]
+    if indexes.__len__() > 0:
+        min_domain = [csp[i].domain.__len__() for i in indexes]
+        indexes_of_min = [i for i in range(min_domain.__len__()) if min(min_domain) == min_domain[i]]
+        max_of_min = [csp[i].neighbors.__len__() for i in indexes_of_min]
+        index = max_of_min.index(max(max_of_min))
+        index = indexes_of_min[index]
+        return indexes[index]
+
+    return None
+
--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import read
 import random
-# from geneticalgorithm import doGA
 from CSP import doCSP
-# from geneticalgorithm import operations
 import sys
 
-if len(sys.argv) != 2:
-        print("Usage: %s input_file" % sys.argv[0])
-        sys.exit()
+# if len(sys.argv) != 2:
+#         print("Usage: %s input_file" % sys.argv[0])
+#         sys.exit()
 
 sys.setrecursionlimit(100000)
 
 
 random.seed(12345678901)
-nodes = read.read_problem(sys.argv[1])
-
+# nodes = read.read_problem(sys.argv[1])
+nodes = read.read_problem("data/gc_4_1")
 # doGA.doGA(nodes)
 doCSP.backtrack_search(nodes)
 
